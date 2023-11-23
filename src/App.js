79698b5aa1126bba2ca3dd4f9a45cf151e14aadd
BowdenK <112501404+BowdenK@users.noreply.github.com>
2023-11-23 21:19:21 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Person from "./Person";
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Site</h1>
+      <h1>My Sitedfdfbdb</h1>
+      <Person />
+    </div>
+  )
+}
+
+export default App;
