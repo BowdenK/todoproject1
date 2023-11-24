2c8feee6ca4e7d9b7c26fb69a8262716d229fabb
BowdenK <112501404+BowdenK@users.noreply.github.com>
2023-11-24 19:10:42 +0000

    input function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Person from "./Person";
+import TodoForm from "./components/TododForm";
 
 const App = () => {
   return (
     <div>
       <h1>My Site</h1>
       <h1>My Sitedfdfbdb</h1>
+      <h1>hdhdhdhd</h1>
       <Person />
+      <TodoForm />
     </div>
   )
 }
